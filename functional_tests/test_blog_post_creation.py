7897e0915bae131a5115bcd30df86676546b9be1
Shivam Sharma <shivams906@gmail.com>
2020-09-21 17:52:47 +0530

    added helper method

--- functional_tests/test_blog_post_creation.py
+++ functional_tests/test_blog_post_creation.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
+import time
+
+MAX_WAIT = 5
 
 
 class BlogTest(LiveServerTestCase):
@@ -15,9 +19,20 @@ class BlogTest(LiveServerTestCase):
     def test_blog_post_creation(self):
         # Edith goes to the home page
         self.browser.get(f"{self.live_server_url}/blogger/")
-        self.assertIn("Home", self.browser.title)
+        wait_for(lambda: self.assertIn("Home", self.browser.title))
         self.fail("finish the test")
 
         # She clicks on the add post link
         # She writes in the content and clicks the publish button
         # She is taken to the newly created post's page
+
+
+def wait_for(function):
+    start_time = time.time()
+    while True:
+        try:
+            return function()
+        except WebDriverException as exception:
+            if time.time() - start_time > MAX_WAIT:
+                raise exception
+            time.sleep(0.5)

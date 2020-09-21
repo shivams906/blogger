ca511a22619632eb7fa0faa85eb70d4db6bee8a3
Shivam Sharma <shivams906@gmail.com>
2020-09-21 17:53:35 +0530

    added tests

---
+++ blogger/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpResponse
+from blogger import views
+
+
+class IndexViewTest(TestCase):
+    def test_url_resolves_to_correct_view_function(self):
+        found = resolve("/blogger/")
+        self.assertEqual(found.func, views.index)
+
+    def test_view_returns_a_valid_response(self):
+        response = self.client.get("/blogger/")
+        self.assertIsInstance(response, HttpResponse)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get("/blogger/")
+        self.assertTemplateUsed(response, "blogger/index.html")

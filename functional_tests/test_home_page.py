a673b090e7a2ddf43c509cc144b50bbf036e9d88
Shivam Sharma <shivams906@gmail.com>
2020-09-26 20:20:39 +0530

    added an FT, linked post from home page to post's page

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -1,4 +1,5 @@
 from .base import *
+from django.utils.text import slugify
 
 
 class HomePageTest(FunctionalTest):
@@ -68,3 +69,40 @@ class HomePageTest(FunctionalTest):
         self.assertIn("edith123", main_content)
 
         meredith_browser.quit()
+
+    def test_post_title_links_to_post_page(self):
+        # Edith goes to the home page
+        self.browser.get(f"{self.live_server_url}/blogger/")
+
+        # She signs up and logs in
+        self.signup(username="edith123", password="top_secret")
+        self.login(username="edith123", password="top_secret")
+
+        # She creates a post and returns to home page
+        wait_for(lambda: self.browser.find_element_by_link_text("Add Post")).click()
+        wait_for(lambda: self.browser.find_element_by_id("id_title")).send_keys("title")
+        wait_for(lambda: self.browser.find_element_by_id("id_content")).send_keys(
+            "content"
+        )
+        wait_for(lambda: self.browser.find_element_by_id("id_submit")).click()
+        wait_for(lambda: self.browser.find_element_by_link_text("Home")).click()
+
+        # She clicks on the title of her post
+        wait_for(lambda: self.browser.find_element_by_link_text("title")).click()
+
+        # She is taken to the post's page
+
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.current_url,
+                f"{self.live_server_url}/blogger/posts/{slugify('title')}/",
+            )
+        )
+
+        wait_for(lambda: self.assertIn("title", self.browser.title))
+        main_content = wait_for(
+            lambda: self.browser.find_element_by_tag_name("main")
+        ).text
+        self.assertIn("title", main_content)
+        self.assertIn("content", main_content)
+        self.assertIn("by edith123", main_content)

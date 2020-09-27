d794e362d6bed71ca2c40fb327086ca1c39e825e
Shivam Sharma <shivams906@gmail.com>
2020-09-27 22:20:32 +0530

    linked posts on author's page to post's page

--- functional_tests/test_author_profile_page.py
+++ functional_tests/test_author_profile_page.py
@@ -60,3 +60,47 @@ class AuthorProfilePageTest(FunctionalTest):
             lambda: self.browser.find_element_by_tag_name("main")
         ).text
         self.assertIn("title", main_content)
+
+    def test_post_title_links_to_post(self):
+        # Edith goes to the home page
+        self.browser.get(f"{self.live_server_url}/blogger/")
+
+        # She signs up and logs in
+        self.signup(username="edith123", password="top_secret")
+        self.login(username="edith123", password="top_secret")
+
+        # She creates a post and returns to home page
+        wait_for(lambda: self.browser.find_element_by_link_text("Add Post")).click()
+        wait_for(lambda: self.browser.find_element_by_id("id_title")).send_keys("title")
+        wait_for(lambda: self.browser.find_element_by_id("id_content")).send_keys(
+            "content"
+        )
+        wait_for(lambda: self.browser.find_element_by_id("id_submit")).click()
+        wait_for(lambda: self.browser.find_element_by_link_text("Home")).click()
+
+        # She goes to her profile page linked by her username
+        wait_for(lambda: self.browser.find_element_by_id("id_user")).click()
+        wait_for(lambda: self.assertIn("edith123's profile", self.browser.title))
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.current_url,
+                f"{self.live_server_url}/blogger/bloggers/edith123/",
+            )
+        )
+
+        # She is shown all posts by her
+        main_content = wait_for(
+            lambda: self.browser.find_element_by_tag_name("main")
+        ).text
+        self.assertIn("title", main_content)
+
+        # She clicks on one of the posts
+        wait_for(lambda: self.browser.find_element_by_link_text("title")).click()
+
+        # She is taken to the post's page
+        wait_for(lambda: self.assertIn("title", self.browser.title))
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.current_url, f"{self.live_server_url}/blogger/posts/title/"
+            )
+        )

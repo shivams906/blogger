fc35e4bc0fa766255cbd91b3fd072e89b47cc97d
Shivam Sharma <shivams906@gmail.com>
2020-09-27 21:22:50 +0530

    edit, delete links are available only to post's author

--- functional_tests/test_post_page.py
+++ functional_tests/test_post_page.py
@@ -88,3 +88,66 @@ class PostPageTest(FunctionalTest):
             lambda: self.browser.find_element_by_tag_name("main")
         ).text
         wait_for(lambda: self.assertNotIn("title", main_content))
+
+    def test_only_authors_can_see_the_link_to_edit_or_delete(self):
+        # Edith goes to the home page
+        self.browser.get(f"{self.live_server_url}/blogger/")
+
+        # She signs up and logs in
+        self.signup(username="edith123", password="top_secret")
+        self.login(username="edith123", password="top_secret")
+
+        # She creates a post and is taekn to post's page
+        wait_for(lambda: self.browser.find_element_by_link_text("Add Post")).click()
+        wait_for(lambda: self.browser.find_element_by_id("id_title")).send_keys("title")
+        wait_for(lambda: self.browser.find_element_by_id("id_content")).send_keys(
+            "content"
+        )
+        wait_for(lambda: self.browser.find_element_by_id("id_submit")).click()
+
+        # She sees the link to edit and delete
+        wait_for(lambda: self.browser.find_element_by_link_text("edit"))
+        wait_for(lambda: self.browser.find_element_by_link_text("delete"))
+
+        # She closes the browser
+        self.browser.quit()
+
+        # Meredith opens her browser
+        self.browser = webdriver.Firefox(firefox_binary="/usr/lib/firefox/firefox")
+
+        # She goes to the homepage
+        self.browser.get(f"{self.live_server_url}/blogger/")
+
+        # She goes to edith's post
+        wait_for(lambda: self.browser.find_element_by_link_text("title")).click()
+
+        # She doesn't see edit or delete link
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text("edit"), []
+            )
+        )
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text("delete"), []
+            )
+        )
+
+        # She signs up and logs in
+        self.signup(username="meredith123", password="top_secret")
+        self.login(username="meredith123", password="top_secret")
+
+        # She goes to edith's post again
+        wait_for(lambda: self.browser.find_element_by_link_text("title")).click()
+
+        # She doesn't see edit or delete link
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text("edit"), []
+            )
+        )
+        wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text("delete"), []
+            )
+        )
